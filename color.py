b84298e04bc7dece0e03522c4e489fc7f08af7d5
Jim Lin <sdf611097@gmail.com>
2018-02-04 16:19:47 +0800

    Support single-opt shorthand

--- color.py
+++ color.py
@@ -6,12 +6,12 @@ from sys import stderr
 ENV_DEBUG_MODE = 'VERBOSE'
 ENV_ENABLE_TIME = 'ENABLE_TIME'
 ENV_LINE_INFO_TO_STDERR = 'LINE_INFO_TO_STDERR'
-def getEnvOpt(envName, default=False):
+def _getEnvOpt(envName, default=False):
     return default if envName not in os.environ else os.environ[envName]
 
-ENABLE_DEBUG_MODE = getEnvOpt(ENV_DEBUG_MODE)
-ENABLE_TIME_PREFIX = getEnvOpt(ENV_ENABLE_TIME)
-ENABLE_LINE_INFO_TO_STDERR = getEnvOpt(ENV_LINE_INFO_TO_STDERR)
+ENABLE_DEBUG_MODE = _getEnvOpt(ENV_DEBUG_MODE)
+ENABLE_TIME_PREFIX = _getEnvOpt(ENV_ENABLE_TIME)
+ENABLE_LINE_INFO_TO_STDERR = _getEnvOpt(ENV_LINE_INFO_TO_STDERR)
 
 timeformat = '[%m-%d %H:%M:%S]'
 
@@ -46,7 +46,21 @@ _colors = {
     'WHITE': '37',
 }
 
+_fontColorForBGs = {
+    'BG_BLACK': 'WHITE',
+    'BG_RED': 'CYAN',
+    'BG_GREEN': 'MAGENTA',
+    'BG_YELLOW': 'BLUE',
+    'BG_BLUE': 'YELLOW',
+    'BG_MAGENTA': 'GREEN',
+    'BG_CYAN': 'RED',
+    'BG_WHITE': 'BLACK',
+}
+
 _CODE_DICT = {**_bgColors, **_effects, **_colors}
+
+def getAllOptions():
+    return _CODE_DICT.keys()
 
 def getPrefix(codes):
     return '\x1b[' + ';'.join(codes) + 'm'
@@ -63,20 +77,29 @@ def _log(codes, *args, **kwargs):
     arguments.append(END)
     print(*arguments, **kwargs)
 
-def cyan(*args):
-    _log([_colors['CYAN']], *args)
-def red(*args):
-    _log([_colors['RED']], *args)
-def italic(*args):
-    _log([_effects['ITALIC']], *args)
+# def cyan(*args):
+#     _log([_colors['CYAN']], *args)
+# def red(*args):
+#     _log([_colors['RED']], *args)
+# def italic(*args):
+#     _log([_effects['ITALIC']], *args)
+
+def _show(title, d):
+    cyan(title)
+    italic(', '.join(d.keys()))
 
 def showOpts():
-    def show(title, d):
-        cyan(title)
-        italic(', '.join(d.keys()))
-    show('BG_COLORS', _bgColors)
-    show('EFFECTS', _effects)
-    show('COLORS', _colors)
+    '''
+    BG_COLORS
+    BG_BLACK, BG_RED, BG_GREEN, BG_YELLOW, BG_BLUE, BG_MAGENTA, BG_CYAN, BG_WHITE
+    EFFECTS
+    RESET, BOLD, FAINT, ITALIC, UNDERLINE
+    COLORS
+    BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
+    '''
+    _show('BG_COLORS', _bgColors)
+    _show('EFFECTS', _effects)
+    _show('COLORS', _colors)
 
 def log(opts, *args):
     optsUpper = [opt.upper() for opt in opts ]
@@ -111,6 +134,40 @@ def debug(opts, *args):
     if ENABLE_DEBUG_MODE:
         log(opts, *args)
 
+def oneOpt(opt):
+    return lambda *args: log([opt], *args)
+
+def bgColorShorthand(bgColor):
+    def withBgColor(*args):
+        try:
+            log([bgColor, _fontColorForBGs[bgColor.upper()]], *args)
+        except KeyError:
+            _show('BG_COLORS', _bgColors)
+    return withBgColor
+
+black = oneOpt('black')
+red = oneOpt('red')
+green = oneOpt('green')
+yellow = oneOpt('yellow')
+blue = oneOpt('blue')
+magenta = oneOpt('magenta')
+cyan = oneOpt('cyan')
+white = oneOpt('white')
+
+bold = oneOpt('bold')
+faint = oneOpt('faint')
+italic = oneOpt('italic')
+underline = oneOpt('underline')
+
+bg_black = bgColorShorthand('bg_black')
+bg_red = bgColorShorthand('bg_red')
+bg_green = bgColorShorthand('bg_green')
+bg_yellow = bgColorShorthand('bg_yellow')
+bg_blue = bgColorShorthand('bg_blue')
+bg_magenta = bgColorShorthand('bg_magenta')
+bg_cyan = bgColorShorthand('bg_cyan')
+bg_white = bgColorShorthand('bg_white')
+
 _log([_colors['RED'], _effects['ITALIC'], _bgColors['BG_GREEN']],1,2,3)
 
 red(3,2,1)
@@ -126,3 +183,8 @@ def test():
 def a():
     test()
 a()
+
+green(123,'abc')
+cyan('aa')
+
+bg_yellow('log with bg yellow')
